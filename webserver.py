90272122771fd682883a645577bf798b25f90cd3
AshOp <110020190+TheAxes@users.noreply.github.com>
2023-01-25 20:19:26 +0530

    Create webserver.py

---
+++ webserver.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def main():
+  return "Made By TheAxes"
+
+def run():
+  app.run(host="0.0.0.0", port=8000)
+
+def keep_alive():
+  server = Thread(target=run)
+  server.start()
